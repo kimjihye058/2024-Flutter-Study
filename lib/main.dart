fa0fd5af8d2952ce4b6cb79daf146d0b0bf2819a
kimjihye058 <s2321@e-mirim.hs.kr>
2024-12-28 11:36:44 +0900

    header 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String? name;   // 물음표가 붙으면 name이라는 parameter가 있을 수도 있고 없을 수도 있음
-
-  Player({required this.name});
-}
-
 void main() {
-  var jennifer = Player(name:"Potato");
-
-  runApp( App());
+  runApp(App());
 }
 
 // commit test11
-class App extends StatelessWidget { 
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          elevation: 10,
-          title: Text('Hello Flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ), 
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Hey, Selena',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.8),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
